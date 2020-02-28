461c1a001e053e1b0cc5e2e4c1da3df1bd968ae7
cousinsr <cousinsr@oregonstate.edu>
2020-02-27 18:04:27 -0800

    Add circleArea() function and associated unit test test cases.
    Add circleArea() function and associated unit test test cases.

--- task.py
+++ task.py
@@ -6,10 +6,27 @@
 # Last modified: 27 February 2020
 # Description:
 # This program implements the three functions listed below that are required by Assignment 5 specifications.
-# circleRadius(radius) - accepts radius of a circle and computes and returns the area
+# circleArea(radius) - accepts radius of a circle and computes and returns the area
 # listFirstLast(list) - accepts a list and returns the first and last element in the list
 # daysBetween(startDate, endDate) - accepts two dates and returns the number of days between the two dates
 
 
+import math
+import sys
+
+
 def firstrun():
     return "success"
+
+
+# This function takes as input the radius of a circle and computes and returns the area of the circle.
+# If numerical input less than zero is provided, the function will output a message and return None.
+# This function expects that a numerical input is provided.
+def circleArea(radius):
+    # Ensure that the input radius is 0 or greater.
+    if radius < 0:
+        print("The radius must be 0 or greater.")
+        return None
+
+    # Calculate and return the area of the circle for the provided radius.
+    return math.pi * math.pow(radius, 2)
--- tests.py
+++ tests.py
@@ -5,11 +5,12 @@
 # https://oregonstate.instructure.com/courses/1750847/assignments/7761140
 # Last modified: 27 February 2020
 # Description:
-# This program implements unit test test cases for the circleRadius(radius), listFirstLast(list), and
+# This program implements unit test test cases for the circleArea(radius), listFirstLast(list), and
 # daysBetween(startDate, endDate) functions found in task.py.
 
 
 import unittest
+import math
 import task
 
 
@@ -22,6 +23,60 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
+    # Test case 1 for circleArea() function
+    # Description:
+    # Check that circleArea() returns expected output when input radius equals zero.
+    def test_circleArea_case1(self):
+        # Initialize the test data.
+        inputRadius = 0
+
+        # Call the function under test.
+        outputArea = task.circleArea(inputRadius)
+
+        # Assert expectations after the function call.
+        self.assertEqual(0, outputArea)
+
+    # Test case 2 for circleArea() function
+    # Description:
+    # Check that circleArea() returns expected output when input radius is less than zero.
+    def test_circleArea_case2(self):
+        # Initialize the test data.
+        inputRadius = -7
+
+        # Call the function under test.
+        outputArea = task.circleArea(inputRadius)
+
+        # Assert expectations after the function call.
+        self.assertIsNone(None, outputArea)
+
+    # Test case 3 for circleArea() function
+    # Description:
+    # Check that circleArea() returns expected output when input radius is one.
+    def test_circleArea_case3(self):
+        # Initialize the test data.
+        inputRadius = 1
+
+        # Call the function under test.
+        outputArea = task.circleArea(inputRadius)
+
+        # Assert expectations after the function call.
+        self.assertEqual(math.pi, outputArea)
+
+    # Test case 4 for circleArea() function
+    # Description:
+    # Check that circleArea() returns expected output when input radius is 777
+    # inclusive.
+    def test_circleArea_case4(self):
+        # Initialize the test data.
+        inputRadius = 777
+        expectedArea = math.pi * math.pow(inputRadius, 2)
+
+        # Call the function under test.
+        outputArea = task.circleArea(inputRadius)
+
+        # Assert expectations after the function call.
+        self.assertEqual(expectedArea, outputArea)
 
 
 if __name__ == '__main__':
